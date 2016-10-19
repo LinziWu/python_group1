502a6f829a17f27a5730b248a5f7bd3ac3667c17
Tobias Gruba <tcgruba@gmail.com>
2016-10-19 23:58:19 +1100

    backing up

--- group.py
+++ group.py
@@ -1,22 +1,25 @@
 import matplotlib as plt
+import math
 
 
 def first_level_functionality(file, y=0, x=0, z=0):
     """
-    Calculates the land area above sea level (z) using a given data file (file),
-    the mean horizontal spacing (x) and mean vertical spacing (y) and reports
-    the answer in absolute terms as well as a percentage of the land
-    area above the current sea level.
+    The first level of funcitonality calculates the land area above a certain
+    sea level (z) using a given data file (file), the mean horizontal spacing
+    (x) and mean vertical spacing (y) between the points. It reports the
+    answer in absolute terms as well as a percentage
+    of the land area above the current sea level.
 
     For example:
     4 inputs:
-            # sydney250m.txt    (data file path)
+            # 'sydney250m.txt'    (file, must be in string form) or, as a full
+            path like 'Users/john/Desktop/sydney250m.txt'
             # y = 0.278     (the mean vertical spacing)
             # x = 0.231     (the mean horizontal spacing)
-            # z = +2 meters     (a sea level height (L >= 0))
+            # z = 2 (meters)    (a sea level height where z >= 0)
     2 outputs:
-            # 1064 square kilometers        (the land area above level L)
-            # 98.87%        (as a percentage of the land area above the current sea level)
+            # 1064 (square kilometers of land area above sea level z)
+            # 98.87%  (percentage of the total land area above sea level z)
 
 
     >>> first_level_functionality('sydney250m.txt', 0.278, 0.231, 2)
@@ -28,36 +31,43 @@ def first_level_functionality(file, y=0, x=0, z=0):
 
     fileobj = open(file,'r')
 
-    count_above_seaLevel = 0
+    count_above_sea_level = 0
     count_total = 0
 
     for row in fileobj:
-        # Split the argument into words using str.split()
+        # Focuses on just the 'z' values of the data file using str.split()
         altitude = float(row.split()[2])
 
         if altitude > 0:
             count_total += 1
 
             if altitude > z:
-                #explain double for loop here
-                count_above_seaLevel += 1
+                count_above_sea_level += 1
+                # counts the amount of values above 'z'
 
-    area_above_seaLevel = int(x * y * count_above_seaLevel)
+    area_above_sea_level = int(x * y * count_above_sea_level)
+    # calculates the area by multiplying the mean horizontal spacing,
+    # mean vertical spacing and number of points above 'z'
 
-    #comment explain this
-    percentage_landArea_above_seaLevel = \
-    100 * round(count_above_seaLevel/count_total,4)
+
+    percentage_land_area_above_sea_level = \
+    100 * round(count_above_sea_level/count_total,4)
+    # calculates the percentage of total land area above 'z' by dividing
+    # the number of points above 'z' by the total number of points and
+    # multiplying that value by 100.
+
 
     fileobj.close()
 
     print(
     "The land area above water in this area at +",
     z,
-    "meters will be",
-    area_above_seaLevel,
+    "meters will",
+    area_above_sea_level,
     "square kilometers, which is",
-    percentage_landArea_above_seaLevel,
-    "% of the current land area above water.")
+    percentage_land_area_above_sea_level,
+    "% of the total current land area above water.")
+
 
 
 # Second level functionality
@@ -95,21 +105,23 @@ def second_level_functionality(file, mean_vertical, mean_horizontal):
     #[1,2,3,4,5,6,7,8,9,10]
 
     for t in range(x_min, x_max):
-        for j in (i, i + x_step):
+        for j in (i, i + x_step): #explain triple for loop
             for row in fileobj:
                 if i < float(row.split()[2]) <= i + x_step:
-                    count_above_seaLevel += 1
+                    count_above_seaLevel += 1 # why triple for loop? seems over the top
+
     points.append(count_above_seaLevel)
 
     area = mean_vertical * mean_horizontal
 
     area_list = [x*area for x in range (points)]
-
     result = new_list_forLevelTwo(area_list)
+
     n = 0
+
     while n < 10:
-        plt.plot(points_list.extend(x_min + n * x_step , result[9 - n])
-        n += 1
+        plt.plot(points_list.extend(x_min + n * x_step , result[9 - n])) # comment explaining this
+        n
 
     plt.ylabel('area_above_water')
     plt.xlabel('sea_level_increase')
@@ -140,30 +152,129 @@ def new_list_forLevelTwo (z):
 def third_level_functionality(file):
     """ docstring goes here """
 
-    fileobj = open (file, 'r')
+    fileobj = open(file, 'r')
 
+    make_x_and_y_lists(file)
+
+    fileobj.close()
+
+def make_y_list(file):
+    """ creates list of all the y values """
+
+    fileobj = open(file, 'r')
+
+    y_values_list = []
+
+    for row in fileobj:
+        # iterates through file for every y value and adds it to a list
+        y_value = float(row.split()[0]) # the left column 'y'
+        y_values_list.append(y_value)
+
+    return y_values_list
+
+    fileobj.close()
+
+def make_x_list(file):
+    """ creates lists of all the x values"""
+
+    fileobj = open(file, 'r')
 
     x_values_list = []
-    y_values_list = []
 
     for row in fileobj:
         # iterates through every x value and adds it to a list
         x_value = float(row.split()[1]) # the middle column 'x'
         x_values_list.append(x_value)
-        # iterates through file for every y value and adds it to a list
-        y_value = float(row.split()[0]) # the left column 'y'
-        y_values_list.append(y_value)
 
-
-    # makes a set of all the y_values to get rid of all the repeat values
-    y_values_set = set(y_values_list)
-
-    # need to figure out how to find spacing value for every different y value
-
-
+    return x_values_list
 
     fileobj.close()
 
+
+def mark_split_point_y(file, count=0):
+    """ marks the first index point where y changes value """
+
+    y_values_list = make_y_list(file) #makes y value list
+
+    first_y_value = y_values_list[count] #stores the first y value on the list
+    # as the reference point to see when it changes
+
+    count = 0
+    for value in y_values_list:
+        if value == first_y_value:
+            count += 1
+            #goes through the y_valus and checks if they are the same,
+            # stops when they change
+    return count
+
+
+
+def mean_horizontal_spacing(file):
+    """ calculate the mean spacing between x-values on the first section
+    of points (the first y value) and returns it expressed in km"""
+
+    x_values_list = make_x_list(file) # makes list of x values
+
+    y_value_change_index_point = mark_split_point_y(file) #finds the
+    # point where the first y value changes
+
+    end_x_value = x_values_list[y_value_change_index_point - 1]
+    # finds the same point on the x value list
+
+    horizontal_spacing = ((end_x_value - x_values_list[0]) / (y_value_change_index_point -1))
+    # computes the horizontal_spacing between points on the first section of x
+    #values using the formula (min - max) / (n -1) where n is the number of
+    #points
+
+    horizontal_spacing_km = (400075/360) * math.cos(horizontal_spacing)
+    #converts that number into km (one arc degree of latitude )
+
+
+    print(end_x_value)
+    print(x_values_list[0])
+    print(y_value_change_index_point - 1)
+    print(horizontal_spacing)
+
+    return horizontal_spacing_km
+
+
+
+def mean_vertical_spacing(file):
+    """ calculates the average spacing between y values on the file """
+
+    y_values_list = make_y_list(file) #makes y value list
+
+    #  makes a set of all the y_values to get rid of all the repeat values
+    y_values_set = set(y_values_list)
+
+    y_values_list = list(y_values_set)
+    y_values_list.sort()
+
+    positive_y_list = []
+
+    for element in y_values_list:
+        positive_element = abs(element)
+        positive_y_list.append(positive_element)
+
+    positive_y_list.sort()
+
+    y_values_list = positive_y_list
+
+    # converts negative list of y_values into a positive list of values with
+    # no repitions
+
+
+    vertical_spacing = ((max(y_values_list) - min(y_values_list)) / (len(y_values_list) - 1))
+
+    vertical_spacing_km = (40007/360) * vertical_spacing
+
+
+    print(max(y_values_list))
+    print(min(y_values_list))
+    print(len(y_values_list))
+    print(vertical_spacing)
+
+    return vertical_spacing_km
 
 
 # Fourth level functionality
@@ -175,88 +286,3 @@ def third_level_functionality(file):
 # sample points that are both above the sea level and adjacent, either
 # vertically, horizontally, or diagnonally in any direction, to be part of the
 # same connected land area.
-
-
-def fourth_level_funtionality (file, h):
-    """ docstring """
-    fileobj = open(path, 'r')
-    read_line = fileobj.readline()
-    y_northest = float(read_line.split()[0])
-    fileobj.close()
-
-
-    # Reopen the file because we've already read one row and the next row will be the second row.
-    fileobj = open(path, 'r')
-    count_row_num = 0
-    count_total_rows = 0        # Use array to represent the map, we need to find the number of rows and columns in the array.
-    for row in fileobj:
-        y_list.append(line_list[0])
-        x_list.append(line_list[1])
-        z_list.append(line_list[2])
-        row_list = [float(row.split()[0]), float(row.split()[1]), float(row.split()[2])]
-
-        count_total_rows += 1
-        if row_list[0] == y_northest:
-            count_row_num += 1
-
-
-    # Make the graph as a array
-    num_ROW = count_row_num
-    num_COL = int(count_total_rows/count_row_num)
-
-
-    count_island = 0;
-    for i in range(0,ROW):
-        for j in range(0, COL):
-            if (Map[i][j]==1 and visited[i][j] == False):       # If a cell with value 1 is not visited yet, then new island found.
-                visit_conneceted_land(visited, Map, i, j)       # Visit all cells in this island
-                count_island += 1                               # Increment island count
-    return count_island
-
-
-def not_out_of_range (visited, Map, i, j):
-     """ docstring """
-
-     if visited[row][col] == False and M[row][col]==1 and row < ROW and col < COL :
-         return True
-     else:
-         return False
-
-
-def visit_conneceted_land(Map, row, col, visited):
-    """ docstring """
-
-        # Use a list to get row and column numbers of 8 neighbors of a given cell
-        # From top to bottom, from left to right, both increment by 1.
-        row_num = [-1, -1, -1,  0,  0,  1, 1, 1]
-        col_num = [-1,  0,  1, -1,  1, -1, 0, 1]
-
-        # Mark this cell as visited
-        visited[row][col] = true;
-
-        # Recur for all connected neighbours
-        for i in range(0,9)
-            if (not_out_of_range(visited, Map, row + row_num[i], col + col_num[i]):
-                visit_conneceted_land(visited, Map, row + row_num[i], col + col_num[i])
-
-
-# Just wanna know the number of the same y divided by the number of all rows
-# 'sydney250m.txt': 24644/202 = 122
-# 'tas2k.txt' : 41410/205 = 202
-def count(path):
-    """ docstring """
-
-    fileobj = open(path, 'r')
-    read_line = fileobj.readline()
-    y_northest = float(read_line.split()[0])
-    fileobj.close()
-
-    total = 0
-    count = 0
-    fileobj = open(path, 'r')
-    for row in fileobj:
-        total += 1
-        if (float(row.split()[0]) == y_northest):
-            count += 1
-
-    return (count,total)
